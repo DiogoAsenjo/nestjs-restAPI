6e2c59d643a5be6079fd6c8ab22396258771e863
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-11 16:30:39 -0300

    Aprendendo a criar um módulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
+//Esse arquivo sempre como o principal, ele irá organizar todos os módulos de aplicação.
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
 })
 export class AppModule {}
