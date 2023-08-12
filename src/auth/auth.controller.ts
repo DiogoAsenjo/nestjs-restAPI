f12a63d06a6f9233259b6072f8e0790d2a0ee6aa
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-12 10:29:13 -0300

    Criando o controller e provider de autenticação

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,5 @@
+//Controllers are responsible for handling incoming requests and returning responses to the client.
+import { Controller } from "@nestjs/common";
+
+@Controller()
+export class AuthController{}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,5 @@
+//Os providers são uma parte fundamental do NestJS e são usados para gerenciar a criação e compartilhamento de instâncias de classes.
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService{}
