a1ff5b1046fccb32b15fd0d58bd9b7695eb50c3f
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-12 10:38:36 -0300

    Linkando o controller e o provider de autenticação

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,8 @@
 //Controllers are responsible for handling incoming requests and returning responses to the client.
 import { Controller } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
 @Controller()
-export class AuthController{}
+export class AuthController{
+    constructor (private authService: AuthService) {} //A ideia por trás disso é que você pode usar o AuthService dentro do AuthController para implementar a lógica relacionada à autenticação.
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,14 @@
 //Os providers são uma parte fundamental do NestJS e são usados para gerenciar a criação e compartilhamento de instâncias de classes.
 import { Injectable } from "@nestjs/common";
 
+//Aqui, na classe AuthService, eu já estou criando os métodos de login e criação de conta, que serão necessários mais pra frente.
 @Injectable({})
-export class AuthService{}
+export class AuthService{
+    login() {
+
+    }
+
+    signup() {
+
+    }
+}
