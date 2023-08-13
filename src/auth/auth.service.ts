7eb6a361ab451905fa20a8b237dc52984791b49c
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-13 11:19:05 -0300

    Criando a lógica do singup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 //Os providers são uma parte fundamental do NestJS e são usados para gerenciar a criação e compartilhamento de instâncias de classes.
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { error } from "console";
 
 //Aqui, na classe AuthService, eu já estou criando os métodos de login e criação de conta, que serão necessários mais pra frente.
 @Injectable({})
@@ -17,15 +19,23 @@ export class AuthService{
         //Gerando o hash do password
         const hash = await argon.hash(dto.password);
         //Salvando o novo usuário no BD
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            });
+    
+            delete user.hash;
+            //Retornando o usuário salvo
+            return user;
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if(error.code === 'P2002') {
+                    throw new ForbiddenException('Email already registered')
+                }
             }
-        });
-
-        delete user.hash;
-        //Retornando o usuário salvo
-        return user;
+        } throw error 
     }
 }
